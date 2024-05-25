e530dd2ac5f5aeedbd21bb5faf8ab1f0d1023a92
Mukunda Parajuli <mukundaparajuli13@gmail.com>
2024-05-25 22:16:43 +0545

    enums

---
+++ 02Basics/enums.ts
@@ -0,0 +1,14 @@
+const enum seatChoice {
+  AISLE,
+  MIDDLE_LEFT,
+  MIDDLE_RIGHT,
+  WINDOW,
+}
+const hcSeat = seatChoice.AISLE;
+
+// we can provide certain options for the seatChoice
+// the first value has 0 value initially
+// we can provide values randomly to the data and the value for other will increase in an order
+// we can use const to avoid scattered codes in js==> only the necessary codes will be generated
+
+export {};
