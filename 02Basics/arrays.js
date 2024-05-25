62bbb6e888e20b2fb59248cf04cab861b1de0a9e
Mukunda Parajuli <mukundaparajuli13@gmail.com>
2024-05-25 20:57:24 +0545

    arrays

---
+++ 02Basics/arrays.js
@@ -0,0 +1,10 @@
+var Actors = [];
+Actors.push("Robert De Niro");
+var allUsers = [];
+allUsers.push({
+    name: "Mukunda Parajuli",
+    age: 21,
+}, {
+    name: "Ramesh Chaudhary",
+    age: 18,
+});
---
+++ 02Basics/arrays.ts
@@ -0,0 +1,27 @@
+const Actors: string[] = [];
+
+Actors.push("Robert De Niro");
+
+type User = {
+  name: string;
+  age: number;
+};
+
+const allUsers: User[] = [];
+
+allUsers.push(
+  {
+    name: "Mukunda Parajuli",
+    age: 21,
+  },
+  {
+    name: "Ramesh Chaudhary",
+    age: 18,
+  }
+);
+
+const MLModel: number[][] = [];
+
+MLModel.push([1, 2, 3], [2, 3, 4]);
+
+export {};
