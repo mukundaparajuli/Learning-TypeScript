fcbe804b13894a7b39fa9cb7300af71411b36390
Mukunda Parajuli <mukundaparajuli13@gmail.com>
2024-05-26 15:54:22 +0545

    basics of classes in typescript

--- 03PureTS/dist/index.js
+++ 03PureTS/dist/index.js
@@ -1,3 +1,14 @@
 "use strict";
-console.log("TypeScript is here!");
-console.log("TypeScript is amazing!");
+// console.log("TypeScript is here!");
+// console.log("TypeScript is amazing!");
+Object.defineProperty(exports, "__esModule", { value: true });
+class User {
+    constructor(name, age, city) {
+        this.email = "";
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+}
+const mukunda = new User("Mukunda Parajuli", 21, "Pokhara");
+mukunda.email = "12";
--- 03PureTS/src/index.ts
+++ 03PureTS/src/index.ts
@@ -1,2 +1,19 @@
-console.log("TypeScript is here!");
-console.log("TypeScript is amazing!");
+// console.log("TypeScript is here!");
+// console.log("TypeScript is amazing!");
+
+class User {
+  name: string;
+  age: number;
+  city: string;
+  readonly email: string = "";
+
+  constructor(name: string, age: number, city: string) {
+    this.name = name;
+    this.age = age;
+    this.city = city;
+  }
+}
+
+const mukunda = new User("Mukunda Parajuli", 21, "Pokhara");
+mukunda.email = "12";
+export {};
