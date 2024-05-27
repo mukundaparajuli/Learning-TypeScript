5bdb5435301982501fb9cf187fc6987a915da5df
Mukunda Parajuli <mukundaparajuli13@gmail.com>
2024-05-27 07:57:26 +0545

    getters and setters in typescript

--- 03PureTS/dist/index.js
+++ 03PureTS/dist/index.js
@@ -2,13 +2,39 @@
 // console.log("TypeScript is here!");
 // console.log("TypeScript is amazing!");
 Object.defineProperty(exports, "__esModule", { value: true });
+// class User {
+//   name: string;
+//   #age: number; // ==> a way to declare a variable private
+//   city: string;
+//   private readonly email: string = "";
+//   constructor(name: string, age: number, city: string) {
+//     this.name = name;
+//     this.#age = age;
+//     this.city = city;
+//   }
+// }
+// const mukunda = new User("Mukunda Parajuli", 21, "Pokhara");
+// mukunda.email = "12";
+// mukunda.email;
+// mukunda.#age;
 class User {
-    constructor(name, age, city) {
-        this.email = "";
+    constructor(name, age, email) {
         this.name = name;
         this.age = age;
-        this.city = city;
+        this.email = email;
+        this.courseCount = 1;
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get getCourseCount() {
+        return this.courseCount;
+    }
+    // setter cannot have a return type annotaion
+    set setCourseCount(courseNumber) {
+        if (courseNumber < 1) {
+            throw new Error("Course no. cannot be less than 1");
+        }
+        this.courseCount = courseNumber;
     }
 }
-const mukunda = new User("Mukunda Parajuli", 21, "Pokhara");
-mukunda.email = "12";
--- 03PureTS/src/index.ts
+++ 03PureTS/src/index.ts
@@ -1,19 +1,40 @@
 // console.log("TypeScript is here!");
 // console.log("TypeScript is amazing!");
 
-class User {
-  name: string;
-  age: number;
-  city: string;
-  readonly email: string = "";
+// class User {
+//   name: string;
+//   #age: number; // ==> a way to declare a variable private
+//   city: string;
+//   private readonly email: string = "";
 
-  constructor(name: string, age: number, city: string) {
-    this.name = name;
-    this.age = age;
-    this.city = city;
+//   constructor(name: string, age: number, city: string) {
+//     this.name = name;
+//     this.#age = age;
+//     this.city = city;
+//   }
+// }
+
+// const mukunda = new User("Mukunda Parajuli", 21, "Pokhara");
+// mukunda.email = "12";
+// mukunda.email;
+// mukunda.#age;
+
+class User {
+  private courseCount: number = 1;
+  constructor(public name: string, public age: number, public email: string) {}
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+  get getCourseCount(): number {
+    return this.courseCount;
+  }
+
+  // setter cannot have a return type annotaion
+  set setCourseCount(courseNumber: number) {
+    if (courseNumber < 1) {
+      throw new Error("Course no. cannot be less than 1");
+    }
+    this.courseCount = courseNumber;
   }
 }
-
-const mukunda = new User("Mukunda Parajuli", 21, "Pokhara");
-mukunda.email = "12";
 export {};
